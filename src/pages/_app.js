7c5fd87fe72b3650e71596987d972d78f1e98949
Gustavo Sales <gsales@blvd.co>
2021-10-11 16:21:38 -0300

    add footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer className="border border-t border-gray-300 py-3 text-center text-xs text-gray-600">
+        Playground built by Gus -{' '}
+        <a
+          href="https://twitter.com/gussalesdev"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-blue-500"
+        >
+          @gussalesdev
+        </a>{' '}
+      </footer>
+    </>
+  );
 }
 
 export default MyApp;
